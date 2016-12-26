768c18a12676d4ece61e16cb1e72d8ad8f3f1b9e
izak <minato128@users.noreply.github.com>
2016-12-26 18:33:49 +0900

    Add ExceptionLogger

---
+++ WebApplication1/WebApplication1/TestExceptionLogger.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics;
+using System.Web.Http.ExceptionHandling;
+using NLog;
+
+namespace WebApplication1
+{
+    public class TestExceptionLogger : ExceptionLogger
+    {
+        private readonly Logger _logger = LogManager.GetCurrentClassLogger();
+
+        public override void Log(ExceptionLoggerContext context)
+        {
+            _logger.Error(context.Exception);
+        }
+    }
+}
